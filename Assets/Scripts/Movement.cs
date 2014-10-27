1afa12f11ed25dd3691069a9c0aea2979cbcdfab
Matt Lagueras <matt.lagueras@stonybrook.edu>
2014-10-27 14:46:37 -0400

    benchmark1

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed = 6f;            // The speed that the player will move at.
+	
+	Vector3 movement;                   // The vector to store the direction of the player's movement.
+
+	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+	
+	void Awake ()
+	{
+	
+		
+
+		playerRigidbody = GetComponent <Rigidbody> ();
+	}
+	
+	
+	void FixedUpdate ()
+	{
+
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+	
+		
+
+		Move (h, v);
+		
+
+	
+	}
+	
+	void Move (float h, float v)
+	{
+
+		movement.Set (h, 0f, v);
+
+		movement = Camera.main.transform.rotation * movement;
+		
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+	
+
+}
+
---
+++ Assets/Scripts/ShootUp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootUp : MonoBehaviour {
+
+	int count = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnParticleCollision(GameObject other) {
+
+		Rigidbody body = other.rigidbody;
+		if (body) {
+			Vector3 direction = Vector3.up;
+			body.AddForce(direction * 5);
+		}
+	}
+}
