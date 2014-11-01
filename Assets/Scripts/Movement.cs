eedd4892fbe5bac66e86b66efa15ec47c2034dbb
Matt Lagueras <matt.lagueras@stonybrook.edu>
2014-11-01 17:26:51 -0400

    working on lvl entrance

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,14 +8,66 @@ public class Movement : MonoBehaviour {
 	Vector3 movement;                   // The vector to store the direction of the player's movement.
 
 	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-	
+
+	public bool onGround = true;
+	private bool jumped = false;
+	public float jumpForce = 1400f;
+
+	private bool disableLeft = false;
+	private bool disableRight = false;
+	private bool disableForward = false;
+	private bool disableBackwards = false;
+
+
+
+
 	void Awake ()
 	{
-	
-		
-
 		playerRigidbody = GetComponent <Rigidbody> ();
 	}
+
+
+	void Update()
+	{
+
+		/*if (playerRigidbody.velocity.y == 0 && !onGround) 
+		{
+			onGround = true;
+		}*/
+
+		if (Input.GetButtonDown ("Jump") && onGround) 
+		{
+			jumped = true;
+		}
+	}
+
+	void OnCollisionStay(Collision collisionInfo)
+	{
+		foreach (ContactPoint contact in collisionInfo.contacts) 
+		{
+			if(contact.otherCollider.gameObject.tag == "Level_Floor")
+			{
+				onGround = true;
+			}
+
+
+
+		}
+	}
+
+	void OnCollisionExit(Collision collisionInfo)
+	{
+		foreach (ContactPoint contact in collisionInfo.contacts) 
+		{
+			if(contact.otherCollider.gameObject.tag == "Level_Floor")
+			{
+				onGround = false;
+				
+			}
+
+		}
+	}
+
 	
 	
 	void FixedUpdate ()
@@ -24,27 +76,52 @@ public class Movement : MonoBehaviour {
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxisRaw ("Vertical");
 
-	
-		
+
+		if (jumped) 
+		{
+			Jump ();
+			jumped = false;
+		}
+
+
 
 		Move (h, v);
 		
 
 	
 	}
+
+
+
 	
 	void Move (float h, float v)
 	{
 
 		movement.Set (h, 0f, v);
 
+		//Debug.Log (movement.ToString ());
+
 		movement = Camera.main.transform.rotation * movement;
+		movement.y = 0f;
 		
 
 		movement = movement.normalized * speed * Time.deltaTime;
 
 		playerRigidbody.MovePosition (transform.position + movement);
 	}
+
+	void Jump()
+	{
+		playerRigidbody.AddForce (jumpForce * Vector3.up);
+	}
+
+	/*void OnGUI () 
+	{
+	
+	GUI.Box (new Rect (10, 10, 100, 50), playerRigidbody.velocity.ToString());
+	GUI.Box (new Rect (10, 30, 100, 50), jumped.ToString());
+	GUI.Box (new Rect (10, 50, 100, 50), onGround.ToString());
+	}*/
 	
 
 }
